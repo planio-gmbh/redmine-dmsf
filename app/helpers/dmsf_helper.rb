edb1ff9fc66f72b0aa7bffc8c3ea58a90de72b0a
Felix Schäfer <felix@plan.io>
2015-07-06 15:44:13 +0200

    Don't store dmsf temp files in /tmp

--- app/helpers/dmsf_helper.rb
+++ app/helpers/dmsf_helper.rb
@@ -22,7 +22,10 @@ require 'digest/md5'
 module DmsfHelper
 
   def self.temp_dir
-    Dir.tmpdir
+    # fixed temp dir for plan.io
+    temp_dir = Rails.root.join('files', (Thread.current[:planio_account] || 'test'), 'dmsf_temp').to_s
+    FileUtils.mkdir_p(temp_dir) unless File.exists?(temp_dir)
+    temp_dir
   end
 
   def self.temp_filename(filename)
